9ccc45c09850ecba7be89c6ac77cb2c4d48f2a45
Alex Swan <Alexander_Swan@fundslibrary.co.uk>
2016-11-21 14:14:26 +0000

    Added watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require("gulp");
 var pug = require("gulp-pug");
 var rename = require("gulp-rename");
+var watch = require("gulp-watch");
 
 gulp.task("build-views", () =>{
     return gulp.src(["./src/views/**/*.pug", "!./src/views/**/_*" ])
-        .pipe(pug({
-            data: {body: "layout.pug"}
-        }))
+        .pipe(pug({}))
         .pipe(gulp.dest("./bin"))
 });
+
+gulp.task("watch-views", () => {
+    return watch(["./src/views/**/*.pug", "!./src/views/**/_*" ])
+        .pipe(pug({ }))
+        .pipe(gulp.dest("./bin"))
+});
+
+gulp.task("watch", ["watch-views"])
 
 gulp.task("default", ["build-views"]);
